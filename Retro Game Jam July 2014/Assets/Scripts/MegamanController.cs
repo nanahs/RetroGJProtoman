6f321900297f276054944ddbe94ab6144b140240
Shanan Dalton <shanan_dalton@yahoo.com>
2014-07-19 05:17:13 -0400

    Megaman animations
    Set up megaman’s animator state system and extended moveable entity for
    him

---
+++ Retro Game Jam July 2014/Assets/Scripts/MegamanController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MegamanController : MoveableEntity{
+
+	private bool firstGroundHit = false;
+	private Animator anim;
+
+	public override void Start(){
+
+		anim = this.GetComponent<Animator>();
+		base.Start();
+
+	}
+
+	public override void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			tryToJump();
+		}
+
+		if(Input.GetKeyDown(KeyCode.K)){
+			tryToShoot();
+		}
+	
+	}
+
+	public override void FixedUpdate(){
+
+		setHorizontal(Input.GetAxis("Horizontal"));
+		setVertical(Input.GetAxis("Vertical"));
+
+		base.FixedUpdate();
+
+		if(grounded && !firstGroundHit){
+			firstGroundHit = true;
+			anim.SetBool("TeleportHitGround", true);
+		}
+
+		if(grounded){
+			anim.SetFloat("moveSpeed", Mathf.Abs(h));
+		}
+
+	}
+}
---
+++ Retro Game Jam July 2014/Assets/Scripts/MoveableEntity.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveableEntity : MonoBehaviour {
+
+	public GameObject smallShot;
+
+	public float moveSpeed = 5f;
+	public float jumpForce = 5f;
+
+	protected float h;
+	protected float v;
+	public bool facingRight = true;
+
+	private Transform ceilingCheck;
+	private Transform groundCheck;
+	private Transform bulletSpawn;
+	private float groundedRadius = .1f;
+	private float ceilingRadius = .1f;
+	public bool grounded = false;
+	public LayerMask whatIsGround;
+
+	private bool jump = false;
+	private bool wantsToJump = false;
+
+	protected bool canShoot = true;
+	private bool shoot = false;
+
+	public virtual void Start() {
+
+		// Setting up references.
+		groundCheck = transform.Find("GroundCheck");
+		ceilingCheck = transform.Find("CeilingCheck");
+	
+	}
+
+	public virtual void Update() {
+
+
+	
+	}
+
+	public virtual void FixedUpdate(){
+		
+		//h = Input.GetAxis("Horizontal");
+		//v = Input.GetAxis("Vertical");
+		
+		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundedRadius, whatIsGround);
+		
+		//Sets the direction that the player needs to face based on the movement
+		if(h > 0 && !facingRight){
+			FlipSprite();
+		}else if(h < 0 && facingRight){
+			FlipSprite();
+		}
+		
+		//Set Play animations for movement
+		rigidbody2D.velocity = new Vector2(h * moveSpeed, rigidbody2D.velocity.y);
+		
+		
+		// If the player should jump...
+		if (grounded && jump) {
+			// Add a vertical force to the player.
+			rigidbody2D.AddForce(new Vector2(0f, jumpForce));
+			jump = false;
+		}
+
+		if(shoot){
+
+		}
+		
+	}
+	
+	private void FlipSprite(){
+		
+		facingRight = !facingRight;
+		
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		
+		transform.localScale = theScale;
+		
+	}
+
+	public void setHorizontal(float horizontal){
+
+		h = horizontal;
+
+	}
+
+	public void setVertical(float vertical){
+
+		v = vertical;
+
+	}
+
+	public float getHorizontal(){
+
+		return h;
+
+	}
+
+	public float getVertical(){
+
+		return v;
+
+	}
+
+	public void tryToJump(){
+
+		if(grounded){
+			jump = true;
+		}
+
+	}
+
+	public void tryToShoot(){
+
+		if(canShoot == true){
+			shoot = true;
+		}
+
+	}
+}
